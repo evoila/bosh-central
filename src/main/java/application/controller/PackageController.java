821aa1ef75332beb6fd6e4a8a54ef61f63425fe2
Jannik Heyl <Jannik.Heyl@googlemail.com>
2018-10-10 12:11:03 +0200

    added CORS[C

--- src/main/java/application/controller/PackageController.java
+++ src/main/java/application/controller/PackageController.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@CrossOrigin()
 @RestController
 @RequestMapping(value = "/package")
 public class PackageController {
